e6ff44f47f241b879e37926a811543620228f20e
olajumokeolarewaju <mrslumeo@gmail.com>
2022-01-13 13:33:41 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var indexValue = 1;
+showImg(indexValue);
+function btn_slide(e) {
+    showImg(indexValue = e);
+}
+function side_slide(e) {
+    showImg(indexValue += e);
+}
+function showImg(e) {
+    var i;
+    const img = document.querySelectorAll('img');
+    const sliders = document.querySelectorAll('.btn-sliders span');
+    if(e > img.length) {
+        indexValue = 1
+    }
+    if(e < 1) {
+        indexValue = img.length
+    }
+    for(i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    for(i = 0; i < sliders.length; i++) {
+        sliders[i].style.background = "black";
+    }
+
+    img[indexValue - 1].style.display = "block";
+    sliders[indexValue - 1].style.background = "white";
+}
